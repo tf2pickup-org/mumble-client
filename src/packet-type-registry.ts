dabcbaa75354f01cf8c894ccdc8ed7fb2481666b
Michał Garapich <michal@garapich.pl>
2022-05-16 23:33:17 +0200

    cleanup

--- src/packet-type-registry.ts
+++ src/packet-type-registry.ts
@@ -28,36 +28,40 @@ import {
 } from '@proto/Mumble';
 import { MessageType } from '@protobuf-ts/runtime';
 
-const packetTypeForName = new Map<MessageType<any>, number>([
-  [Version, 0],
-  [UDPTunnel, 1],
-  [Authenticate, 2],
-  [Ping, 3],
-  [Reject, 4],
-  [ServerSync, 5],
-  [ChannelRemove, 6],
-  [ChannelState, 7],
-  [UserRemove, 8],
-  [UserState, 9],
-  [BanList, 10],
-  [TextMessage, 11],
-  [PermissionDenied, 12],
-  [ACL, 13],
-  [QueryUsers, 14],
-  [CryptSetup, 15],
-  [ContextActionModify, 16],
-  [ContextAction, 17],
-  [UserList, 18],
-  [VoiceTarget, 19],
-  [PermissionQuery, 20],
-  [CodecVersion, 21],
-  [UserStats, 22],
-  [RequestBlob, 23],
-  [ServerConfig, 24],
-  [SuggestConfig, 25],
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type AnyMessage = MessageType<any>;
+
+// https://buildmedia.readthedocs.org/media/pdf/mumble-protocol/latest/mumble-protocol.pdf
+const packetNameForType = new Map<number, AnyMessage>([
+  [0, Version],
+  [1, UDPTunnel],
+  [2, Authenticate],
+  [3, Ping],
+  [4, Reject],
+  [5, ServerSync],
+  [6, ChannelRemove],
+  [7, ChannelState],
+  [8, UserRemove],
+  [9, UserState],
+  [10, BanList],
+  [11, TextMessage],
+  [12, PermissionDenied],
+  [13, ACL],
+  [14, QueryUsers],
+  [15, CryptSetup],
+  [16, ContextActionModify],
+  [17, ContextAction],
+  [18, UserList],
+  [19, VoiceTarget],
+  [20, PermissionQuery],
+  [21, CodecVersion],
+  [22, UserStats],
+  [23, RequestBlob],
+  [24, ServerConfig],
+  [25, SuggestConfig],
 ]);
 
-const packetNameForType = new Map<number, MessageType<any>>();
+const packetTypeForName = new Map<AnyMessage, number>();
 for (const [key, value] of packetTypeForName.entries()) {
   packetNameForType.set(value, key);
 }
@@ -66,6 +70,6 @@ export const packetForType = (type: number) => {
   return packetNameForType.get(type);
 };
 
-export const packetType = (packet: MessageType<any>) => {
+export const packetType = (packet: AnyMessage) => {
   return packetTypeForName.get(packet);
 };
