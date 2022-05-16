b66ea2967a4b27482db37de9b29ce6606e9f865c
Michał Garapich <michal@garapich.pl>
2022-05-17 01:01:29 +0200

    fix(types): export errors (#16)

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { ChannelManager } from './channel-manager';
 export { Channel } from './channel';
 export { UserManager } from './user-manager';
 export { User } from './user';
+export * from './errors';
