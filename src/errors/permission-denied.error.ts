78670254da2496182ec45a424efbab461ed78d1e
Michał Garapich <michal@garapich.pl>
2022-05-19 14:24:32 +0200

    fix(*): better PermissionDeniedError message (#24)

--- src/errors/permission-denied.error.ts
+++ src/errors/permission-denied.error.ts
@@ -1,11 +1,56 @@
-import { PermissionDenied } from '@proto/Mumble';
+import { PermissionDenied, PermissionDenied_DenyType } from '@proto/Mumble';
+
+const toString = (permissionDenied: PermissionDenied): string => {
+  switch (permissionDenied.type) {
+    case PermissionDenied_DenyType.Text:
+      return permissionDenied.reason ?? 'unknown reason';
+
+    case PermissionDenied_DenyType.Permission:
+      return 'permission denied';
+
+    case PermissionDenied_DenyType.SuperUser:
+      return 'cannot modify SuperUser';
+
+    case PermissionDenied_DenyType.ChannelName:
+      return 'invalid channel name';
+
+    case PermissionDenied_DenyType.TextTooLong:
+      return 'text message too long';
+
+    case PermissionDenied_DenyType.H9K:
+      return 'the flux capacitor was spelled wrong';
+
+    case PermissionDenied_DenyType.TemporaryChannel:
+      return 'operation not permitted in temporary channel';
+
+    case PermissionDenied_DenyType.MissingCertificate:
+      return 'operation requires certificate';
+
+    case PermissionDenied_DenyType.UserName:
+      return 'invalid username';
+
+    case PermissionDenied_DenyType.ChannelFull:
+      return 'channel is full';
+
+    case PermissionDenied_DenyType.NestingLimit:
+      return 'channels are nested too deeply';
+
+    case PermissionDenied_DenyType.ChannelCountLimit:
+      return 'maximum channel count reached';
+
+    case PermissionDenied_DenyType.ChannelListenerLimit:
+      return 'amount of listener objects for this channel has been reached';
+
+    case PermissionDenied_DenyType.UserListenerLimit:
+      return 'amount of listener proxies for the user has been reached';
+
+    default:
+      return 'unknown reason';
+  }
+};
 
 export class PermissionDeniedError extends Error {
   constructor(public readonly permissionDenied: PermissionDenied) {
-    let reason = 'permission denied';
-    if (permissionDenied.reason) {
-      reason = permissionDenied.reason;
-    }
-    super(reason);
+    super(`permission denied (${toString(permissionDenied)})`);
   }
 }
