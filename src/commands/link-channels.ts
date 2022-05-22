360200ee3a7f644365f623fea5b7f6066b84587b
Michał Garapich <michal@garapich.pl>
2022-05-22 21:07:39 +0200

    fix(*): linkChannels command timeout

--- src/commands/link-channels.ts
+++ src/commands/link-channels.ts
@@ -1,7 +1,8 @@
+import { CommandTimeout } from '@/config';
 import { MumbleSocket } from '@/mumble-socket';
 import { filterPacket } from '@/rxjs-operators/filter-packet';
 import { ChannelState } from '@proto/Mumble';
-import { filter, take } from 'rxjs';
+import { filter, race, take, timer } from 'rxjs';
 
 export const linkChannels = async (
   socket: MumbleSocket,
@@ -9,13 +10,14 @@ export const linkChannels = async (
   targetChannelId: number,
 ): Promise<void> => {
   return new Promise(resolve => {
-    socket.packet
-      .pipe(
+    race(
+      socket.packet.pipe(
         filterPacket(ChannelState),
         filter(channelSync => channelSync.channelId === channelId),
         take(1),
-      )
-      .subscribe(() => resolve());
+      ),
+      timer(CommandTimeout),
+    ).subscribe(() => resolve());
     socket.send(
       ChannelState,
       ChannelState.create({ channelId, linksAdd: [targetChannelId] }),
