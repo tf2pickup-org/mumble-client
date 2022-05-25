8cd80cfcb88a725b9de82a124dc57e0d0868b464
Michał Garapich <michal@garapich.pl>
2022-05-25 02:21:52 +0200

    fix(*): fix commands' error handling (#39)

--- src/commands/remove-channel.ts
+++ src/commands/remove-channel.ts
@@ -1,28 +1,49 @@
-import { PermissionDeniedError } from '@/errors';
+import { CommandTimeout } from '@/config';
+import { CommandTimedOutError, PermissionDeniedError } from '@/errors';
 import { MumbleSocket } from '@/mumble-socket';
 import { filterPacket } from '@/rxjs-operators/filter-packet';
-import { ChannelRemove, PermissionDenied } from '@tf2pickup-org/mumble-protocol';
-import { filter, race, take } from 'rxjs';
+import {
+  ChannelRemove,
+  PermissionDenied,
+} from '@tf2pickup-org/mumble-protocol';
+import {
+  concatMap,
+  filter,
+  lastValueFrom,
+  map,
+  race,
+  take,
+  throwError,
+  timer,
+} from 'rxjs';
 
 export const removeChannel = async (
   socket: MumbleSocket,
   channelId: number,
-): Promise<number> =>
-  new Promise((resolve, reject) => {
+): Promise<number> => {
+  const ret = lastValueFrom(
     race(
       socket.packet.pipe(
         filterPacket(ChannelRemove),
         filter(channelRemove => channelRemove.channelId === channelId),
         take(1),
+        map(channelRemove => channelRemove.channelId),
       ),
-      socket.packet.pipe(filterPacket(PermissionDenied), take(1)),
-    ).subscribe(packet => {
-      if (PermissionDenied.is(packet)) {
-        reject(new PermissionDeniedError(packet));
-      } else {
-        resolve(packet.channelId as number);
-      }
-    });
-
-    socket.send(ChannelRemove, ChannelRemove.create({ channelId }));
-  });
+      socket.packet.pipe(
+        filterPacket(PermissionDenied),
+        filter(permissionDenied => permissionDenied.channelId === channelId),
+        take(1),
+        concatMap(permissionDenied =>
+          throwError(() => new PermissionDeniedError(permissionDenied)),
+        ),
+      ),
+      timer(CommandTimeout).pipe(
+        concatMap(() =>
+          throwError(() => new CommandTimedOutError('removeChannel')),
+        ),
+      ),
+    ),
+  );
+  socket.send(ChannelRemove, ChannelRemove.create({ channelId }));
+  return ret;
+};
