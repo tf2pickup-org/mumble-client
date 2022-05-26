b069008bc30bc43af6f86f0adb365c9a3269d047
Michał Garapich <michal@garapich.pl>
2022-05-26 12:54:03 +0200

    fix(*): add setSelfDeaf command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -3,5 +3,6 @@ export { fetchChannelPermissions } from './fetch-channel-permissions';
 export { linkChannels } from './link-channels';
 export { moveUserToChannel } from './move-user-to-channel';
 export { removeChannel } from './remove-channel';
+export { setSelfDeaf } from './set-self-deaf';
 export { setSelfMute } from './set-self-mute';
 export { unlinkChannels } from './unlink-channels';
